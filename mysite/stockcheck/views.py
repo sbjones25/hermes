1ec16def972fa880074fe40f47e91546bfdfd5f8
sbjones25 <stephen@jonesvsfx.com>
2016-05-25 14:47:40 -0700

    updating with market update information

--- mysite/stockcheck/views.py
+++ mysite/stockcheck/views.py
@@ -4,6 +4,18 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+import urllib2
+from BeautifulSoup import BeautifulSoup
 
 def index(request):
-    return HttpResponse("Hello, world. Stockcheck Activated.")
+    page = urllib2.urlopen("http://finance.yahoo.com/market-overview/")
+    soup = BeautifulSoup(page)
+    colWrap = soup.findAll('div', {"class":'ColWrap'})[0]
+    marketUpdate = colWrap.findChildren('div',{"class":"bd"})[0]
+
+    message = ''
+    for p in marketUpdate.findAll('p')[:-1]:
+        message += str(p.text)
+    if not message:
+        message = "Counld not retieve market data."
+    return HttpResponse(message)
